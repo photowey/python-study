218456a0d661709a49fb060659664102b9287de8
photowey <photowey@gmail.com>
2021-01-08 23:27:45 +0800

    feat 1.add NationalArea.py

---
+++ src/main/python/spider/NationalArea_Test.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+# ---------------------------------------------
+# @file NationalArea.py
+# @description NationalArea_Test
+# @author WcJun
+# @date 2021/01/08
+# ---------------------------------------------
+
+import src.main.python.spider.NationalArea as NationalArea
+
+if __name__ == "__main__":
+    NationalArea.main()
